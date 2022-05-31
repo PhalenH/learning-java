8868517a636976a7710693683e583ebadbd557a3
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-31 14:03:12 -0400

    finished 5/8

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
+    public static double employeeSalary(double hours, double wage, int vaca){
+        double weekly = hours * wage;
+        double losses = vaca * 8 * hours;
+        double salary = (weekly * 52) - losses;
+        return salary;
+    }
     public static void main(String[] args) {
-        
+       double yearly = employeeSalary(40, 35, 5);
+        System.out.println(yearly);
     }
 
 }
